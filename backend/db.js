61423051045795f7e28ebca336aab63a8a3cb6dd
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-18 23:17:41 +0530

    Create User mongoose schema

--- backend/db.js
+++ backend/db.js
@@ -2,4 +2,39 @@ const mongoose = require('mongoose')
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI);
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+}
