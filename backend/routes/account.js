a46af7711b8c8edaf6c0f5f148a8994a49832982
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-25 22:22:19 +0530

    Endpoint for user to get their balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,7 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const authMiddleware = require('../middleware');
+const { Account } = require('../db');
 
 const router = express.Router();
 
@@ -6,4 +9,17 @@ router.get("/",(req,res)=>{
     res.send("Hello from account");
 });
 
+router.get("/balance", authMiddleware, async(req, res)=>{
+    try{
+        const account = await Account.findOne({ userId: req.userId}); 
+        res.status(200).json({
+            message: `Your current balance is ₹${account.balance}`
+        })
+    } catch(e){
+        console.error(e);
+        res.status(400).json({
+            message: "Could not get balance."
+        })
+    }
+})
 module.exports = router
