bc6adacb0b3d9cbfa3bc76a40affb06fa37e1c3e
MANISHA BOSE <manishabose.2001@gmail.com>
2025-03-08 00:17:26 +0530

    Connect signup frontend with backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,20 +3,41 @@ import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/Subheading";
 import { ButtonWarning } from "../components/ButtonWarning"
+import { useState } from "react";
+import axios from "axios";
 
 export function Signup(){
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("")
     return(
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-100 text-center p-2 h-max px-4">
                     <Heading label={"Sign up"}/>
                     <SubHeading label={"Enter your information to create an account"}/>
-                    <InputBox label={"First Name"} placeholder={"Manisha"}/>
-                    <InputBox label={"Last Name"} placeholder={"Bose"}/>
-                    <InputBox label={"Email"} placeholder={"manisha@example.com"}/>
-                    <InputBox label={"Password"} placeholder={"******"}/>
+                    <InputBox onChange={e =>{
+                        setFirstName(e.target.value);
+                    }} label={"First Name"} placeholder={"Manisha"}/>
+                    <InputBox onChange={e =>{
+                        setLastName(e.target.value);
+                    }} label={"Last Name"} placeholder={"Bose"}/>
+                    <InputBox onChange={e =>{
+                        setUsername(e.target.value);
+                    }} label={"Email"} placeholder={"manisha@example.com"}/>
+                    <InputBox onChange={e =>{
+                        setPassword(e.target.value);
+                    }} label={"Password"} placeholder={"******"}/>
                     <div className="pt-4">
-                        <Button label={"Sign up"}/>
+                        <Button onClick={async()=>{
+                            const response = await axios.post("http://localhost:3000/api/v1/user/signup",{
+                                firstName,
+                                lastName,
+                                username,
+                                password
+                            })
+                        }}label={"Sign up"}/>
                     </div>
                     <ButtonWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
                 </div>
